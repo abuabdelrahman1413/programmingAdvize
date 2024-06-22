f3b6a226ab5e985f7d74471c9c3b43c8ed3be463
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-22 16:16:10 +0200

    count vowel letters

---
+++ 07-problem_solving_levl_3/33-countVowel.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/**
+ * Reads a string from the standard input.
+ *
+ * @return the string entered by the user
+ *
+ * @throws None
+ */
+string ReadString()
+{
+	string str;
+	cout << "Enter a string: ";
+	getline(cin, str);
+	return str;
+}
+
+/**
+ * Checks if the given character is a vowel.
+ *
+ * @param element The character to check.
+ *
+ * @return True if the character is a vowel, false otherwise.
+ *
+ * @throws None
+ */
+bool isVowel(char element)
+{
+  char vowelArr[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+  for (short i = 0; i < 10; i++)
+  {
+    if (vowelArr[i] == toupper(element) || tolower(element) == vowelArr[i])
+      return true;
+  }
+  return false;
+}
+
+/**
+ * Counts the number of vowels in the given string.
+ *
+ * @param str The string to check.
+ *
+ * @return The number of vowels in the string.
+ *
+ * @throws None
+ */
+short CountVowel(string str)
+{
+  short count = 0;
+  for (short i = 0; i < str.length(); i++)
+  {
+    if (isVowel(str[i]))
+      count++;
+  }
+  return count;
+}
+
+/**
+ * Main function
+ *
+ * @param argc
+ * @param argv
+ *
+ * @return 0 on exit
+ */
+int main(int argc, char *argv[])
+{
+  cout << "Enter a string: ";
+  cout << CountVowel(ReadString()) << endl;
+  return 0;
+}
