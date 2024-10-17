c0f8b549ad93676d0c43c302a41685d971efb722
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-10-17 06:58:03 +0200

    problem 62

---
+++ 08-problem_solving_levl_4/62-vlidateDate.cpp
@@ -0,0 +1,144 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct sDate {
+  short Year;
+  short Month;
+  short Day;
+};
+
+struct sPeriod {
+  sDate Start;
+  sDate End;
+};
+
+short ReadNumber(string message) {
+  short number;
+  cout << message;
+  cin >> number;
+  return number;
+}
+
+sDate ReadDate() {
+  sDate Date;
+  Date.Day = ReadNumber("Enter day: ");
+  Date.Month = ReadNumber("Enter month: ");
+  Date.Year = ReadNumber("Enter year: ");
+
+  return Date;
+}
+
+sPeriod ReadPeriod() {
+  sPeriod Period;
+  cout << "Enter start date: " << endl;
+  Period.Start = ReadDate();
+  cout << "Enter end date: " << endl;
+  Period.End = ReadDate();
+  return Period;
+}
+
+short DayOfWeekOrder(short day, short month, short year) {
+  int a = (14 - month) / 12;
+  int y = year - a;
+  int m = month + 12 * a - 2;
+
+  int d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+  return d;
+}
+
+short DayOfWeekOrder(sDate Date) {
+  return DayOfWeekOrder(Date.Day, Date.Month, Date.Year);
+}
+
+void PrintDate(short day, short month, short year) {
+  cout << day << "/" << month << "/" << year << endl;
+}
+
+bool isLeapYear(int Year) {
+  return Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0;
+}
+
+int NumberOfDaysInMonth(int Year, int Month) {
+  if (Month < 1 || Month > 12)
+    return -1;
+  int NumberOfDaysInMonth[12] = {31, 28, 31, 30, 31, 30,
+                                 31, 31, 30, 31, 30, 31};
+  return (Month == 2) ? (isLeapYear(Year) ? 29 : 28)
+                      : NumberOfDaysInMonth[Month - 1];
+}
+
+bool isLastDayInMonth(sDate Date) {
+  short DaysInMonth = NumberOfDaysInMonth(Date.Year, Date.Month);
+  return Date.Day == DaysInMonth;
+}
+
+bool isLastMonth(short Month) { return Month == 12; }
+sDate IncreaseDateByOneDay(sDate Date) {
+  if (isLastDayInMonth(Date)) {
+    if (isLastMonth(Date.Month)) {
+      Date.Day = 1;
+      Date.Month = 1;
+      Date.Year++;
+    } else {
+      Date.Day = 1;
+      Date.Month++;
+    }
+  } else {
+    Date.Day++;
+  }
+
+  return Date;
+}
+
+// bool IsDate1LessThanDate2(sDate Date1, sDate Date2)
+// {
+//     return (Date1.Year < Date2.Year ? true : ((Date1.Year == Date2.Year ?
+//     (Date1.Month < Date2.Month ? true: (Date1.Month == Date2.Month ?
+//     Date1.Day < Date2.Day : false)) : false)));
+// }
+
+bool IsDate1LessThanDate2(sDate Date1, sDate Date2) {
+  return (Date1.Year < Date2.Year)
+             ? true
+             : ((Date1.Year == Date2.Year)
+                    ? (Date1.Month < Date2.Month
+                           ? true
+                           : (Date1.Month == Date2.Month ? Date1.Day < Date2.Day
+                                                         : false))
+                    : false);
+}
+
+int DifferenceInDays(sDate Date1, sDate Date2, bool IncludeEndDay = false) {
+  int DiffereceInDays = 0;
+  while (IsDate1LessThanDate2(Date1, Date2)) {
+    DiffereceInDays++;
+    Date1 = IncreaseDateByOneDay(Date1);
+  }
+
+  return IncludeEndDay ? ++DiffereceInDays : DiffereceInDays;
+}
+
+
+bool isValidDate(sDate Date) {
+    if (Date.Day < 1 || Date.Day > 31)
+        return false;
+    if (Date.Month < 1 || Date.Month > 12)
+        return false;
+    if (Date.Year < 1)
+        return false;
+    if (Date.Day > NumberOfDaysInMonth(Date.Year, Date.Month))
+        return false;
+    return true;
+}
+
+int main(void) {
+
+    sDate Date1 = ReadDate();
+    if (!isValidDate(Date1))
+        cout << "\n\nInvalid Date" << endl;
+    else
+        cout << "\n\nValid Date" << endl;
+  return 0;
+}
